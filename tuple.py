0f4eb1379fee7b84ce4ddcdb7c8f8b728613c3ee
Onur Koçak <98240992+onurkck34@users.noreply.github.com>
2022-03-30 20:07:36 +0300

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,12 @@
+tupleDeger = ("Onur",)
+print(type(tupleDeger))
+print(tupleListe[1:2])
+print(liste[1:2])
+print(tupleListe[-2])
+print(liste[-2])
+
+print(type(liste))
+print(tupleListe)
+print(liste)
+print(len(tupleListe))
+print(len(liste))
